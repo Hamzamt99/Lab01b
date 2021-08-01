96af296cf40fdf46a57b0e905730271abf24441d
Hamzamt99 <tamarihamza4@gmail.com>
2021-08-01 17:19:04 +0300

    Updated

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let Name = prompt("Enter your Name:");
+
+let ID = prompt("Enter your ID:");
+
+let Major = prompt("Enter your Major:");
+
+let Salary = prompt("Enter your Salary:");
+
+document.write(`<h2>${Name + ' ' + ID+  ' **and your Major is ' + Major}</h2>`);
+document.write(`<h2>${Salary + '  Thats good  ' }</h2>`);
+
+
+
